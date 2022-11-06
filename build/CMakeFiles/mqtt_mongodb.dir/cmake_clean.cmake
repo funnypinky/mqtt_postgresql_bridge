5e4a496490efb531e471d2cf2116cb5b53cb1700
funnypinky <steffenhasler@googlemail.com>
2022-11-06 20:07:44 +0100

    init commit

---
+++ build/CMakeFiles/mqtt_mongodb.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mqtt_mongodb.dir/main.cpp.o"
+  "CMakeFiles/mqtt_mongodb.dir/main.cpp.o.d"
+  "mqtt_mongodb"
+  "mqtt_mongodb.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mqtt_mongodb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/mqtt_mongodb.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for mqtt_mongodb.
+# This may be replaced when dependencies are built.
