bce106311ccb1afda43b083a00405241406c5608
funnypinky <steffenhasler@googlemail.com>
2022-11-08 20:34:03 +0100

    remove build

--- build/VersionInfo.h
+++
@@ -1,7 +0,0 @@
-#ifndef VERSIONINFO_H
-#define VERSIONINFO_H
-
-#define BUILD_TIMESTAMP "2022-11-08T07:06:35Z"
-#define CLIENT_VERSION  "1.3.11"
-
-#endif /* VERSIONINFO_H */
--- build/lib/paho.mqtt.c/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/office/source/cpp/mqtt_mongodb_bridge/lib/paho.mqtt.c
-# Build directory: /home/office/source/cpp/mqtt_mongodb_bridge/build/lib/paho.mqtt.c
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("test")
